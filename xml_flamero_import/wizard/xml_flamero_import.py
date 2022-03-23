31e1516c455f8374647eb4eabd4a4436a9bc3d6b
Manuel Calero <manuelcalerosolis@gmail.com>
2022-03-23 21:01:28 +0100

    revisado

--- xml_flamero_import/wizard/xml_flamero_import.py
+++ xml_flamero_import/wizard/xml_flamero_import.py
@@ -1,12 +1,9 @@
 import base64
 import logging
-import re
-import xml.dom.minidom
 from lxml import etree
-from io import StringIO
 
 from odoo import _, api, fields, models
-from requests import auth, exceptions
+from odoo.exceptions import UserError
 
 
 _logger = logging.getLogger(__name__)
@@ -20,48 +17,43 @@ class XmlFlameroImport(models.TransientModel):
         required=False,
         help="Get you data from xml file.",
     )
-    filename = fields.Char()
+    invoice_file = fields.Binary(
+        string="PDF or XML Invoice"
+    )
+    invoice_filename = fields.Char(
+        string="Filename"
+    )
+
+    @api.model
+    def get_parsed_invoice(self, invoice_file_b64):
+        assert invoice_file_b64, "No invoice file"
+        assert isinstance(invoice_file_b64, bytes)
+        file_data = base64.b64decode(invoice_file_b64)
+
+        print("*"*80)
+        print("file_data:", file_data)
+        print("*"*80)
+
+        try:
+            xml_root = etree.fromstring(file_data)
+        except Exception as e:
+            raise UserError(_("This XML file is not XML-compliant. Error: %s") % e)
+
+        pretty_xml_bytes = etree.tostring(
+                xml_root, pretty_print=True, encoding="UTF-8", xml_declaration=True
+            )
+
+        for element in xml_root:
+            print("*" * 80)
+            print("element:", element)
+            for e in element:
+                print("e:", e)
+            print("*" * 80)
+
+        print("*"*80)
+        print("pretty_xml_bytes:", pretty_xml_bytes)
+        print("*"*80)
 
     def import_file(self):
-        """ Process the file chosen in the wizard, create bank statement(s) and go to reconciliation. """
         self.ensure_one()
-
-        print("paso a importar el fichero")
-
-        # datasource = open(self.filename)
-        # datasource = b64decode(self.filename)
-        # if data_file_agentes:
-        #     # self._import_agentes(data_file_agentes)
-        #     print(datasource)
-        #     content = xml.dom.minidom.parseString(datasource)
-        #     print(content)
-
-        print(self.filename)
-        generated_facturae = etree.fromstring(base64.urlsafe_b64decode(self.filename))
-        print(generated_facturae)
-
-        # self.assertEqual(
-        #     generated_facturae.xpath(
-        #         "/fe:Facturae/Parties/SellerParty/TaxIdentification/"
-        #         "TaxIdentificationNumber",
-        #         namespaces={"fe": self.fe},
-        #     )[0].text,
-        #     self.env.ref("base.main_company").vat,
-        # )
-
-        # for val in ["w:p", "w:h", "text:list"]:
-        #     for element in content.getElementsByTagName(val):
-        #         buf += textToString(element) + "\n"
-
-    def decode_base64(data, altchars=b'+/'):
-        """Decode base64, padding being optional.
-
-        :param data: Base64 data as an ASCII byte string
-        :returns: The decoded byte string.
-
-        """
-        data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
-        missing_padding = len(data) % 4
-        if missing_padding:
-            data += b'='* (4 - missing_padding)
-        return b64decode(data, altchars)
+        self.get_parsed_invoice(self.invoice_file)
