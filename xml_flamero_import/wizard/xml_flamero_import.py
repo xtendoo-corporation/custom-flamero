52608a3ed836ce65aeeb3cac866f7c812203a0ed
Manuel Calero <manuelcalerosolis@gmail.com>
2022-03-29 08:32:27 +0200

    lectura xml

--- xml_flamero_import/wizard/xml_flamero_import.py
+++ xml_flamero_import/wizard/xml_flamero_import.py
@@ -29,30 +29,24 @@ class XmlFlameroImport(models.TransientModel):
         assert invoice_file_b64, "No invoice file"
         assert isinstance(invoice_file_b64, bytes)
         file_data = base64.b64decode(invoice_file_b64)
-
-        print("*"*80)
-        print("file_data:", file_data)
-        print("*"*80)
-
         try:
             xml_root = etree.fromstring(file_data)
         except Exception as e:
             raise UserError(_("This XML file is not XML-compliant. Error: %s") % e)
 
-        pretty_xml_bytes = etree.tostring(
-                xml_root, pretty_print=True, encoding="UTF-8", xml_declaration=True
-            )
-
-        for element in xml_root:
-            print("*" * 80)
-            print("element:", element)
-            for e in element:
-                print("e:", e)
-            print("*" * 80)
-
-        print("*"*80)
-        print("pretty_xml_bytes:", pretty_xml_bytes)
-        print("*"*80)
+        for entry in xml_root.xpath('ns:entry', namespaces={"ns": "http://www.w3.org/2005/Atom"}):
+            content = entry.find('{http://www.w3.org/2005/Atom}content')
+            properties = content.xpath("m:properties", namespaces={
+                "m": "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"})
+            email = properties[0].xpath("d:E_mail", namespaces={
+                "d": "http://schemas.microsoft.com/ado/2007/08/dataservices"})
+            if email is not None:
+                nombre = properties[0].xpath("d:Nombre",
+                                            namespaces={"d": "http://schemas.microsoft.com/ado/2007/08/dataservices"})
+                print("*"*80)
+                print("email", email[0].text)
+                print("nombre", nombre[0].text)
+                print("*"*80)
 
     def import_file(self):
         self.ensure_one()
