5506e5c4ec384caa252744fbd0ad85dab5acd39d
dario <dacrumau@gmail.com>
2022-10-10 14:14:15 +0200

    varios

--- xml_flamero_import/__manifest__.py
+++ xml_flamero_import/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Importation from XML",
     "category": "Project",
     "version": "15.0.1.0",
-    "depends": ["base"],
+    "depends": ["base","mass_mailing"],
     "description": """
         Wizard to Import from XML files.
         """,
@@ -11,6 +11,7 @@
         "wizard/xml_flamero_import.xml",
         "views/xml_flamero_import.xml"
     ],
+    'application': True,
     "installable": True,
     "auto_install": True,
 }
